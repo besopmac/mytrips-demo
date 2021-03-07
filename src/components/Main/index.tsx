1d4eed7b8c1ac01247dc62b8e0cdff18944f7b7d
Bruno N. Campos <bruno@mambowifi.com>
2021-03-07 20:53:49 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem bamba" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>TypeScript, React, Next.js Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="" />
+  </S.Wrapper>
+)
+
+export default Main
