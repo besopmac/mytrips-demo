2a9448db26cbf4bda3392a8ac1b9c7032051c8d1
Bruno N. Campos <bruno@mambowifi.com>
2021-03-07 20:59:37 -0300

    Adaptando boilerplate ao projeto

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,3 @@
-import * as S from './styles'
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo src="/img/logo.svg" alt="Imagem bamba" />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>TypeScript, React, Next.js Styled Components</S.Description>
-    <S.Illustration src="/img/hero-illustration.svg" alt="" />
-  </S.Wrapper>
-)
+const Main = () => <h1>NextJS na Prática</h1>
 
 export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next.js na Prática - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React,
-          Next.js and Styled Components"
+          content="A simple project starter to show my favorite spots."
         />
       </Head>
       <GlobalStyles />
