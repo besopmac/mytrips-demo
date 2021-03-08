93a739f863942d4271a4e75e43635c7204d84b3f
Bruno N. Campos <bruno@mambowifi.com>
2021-03-08 01:14:41 -0300

    Configurando Headless CMS

---
+++ src/templates/About/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Content = styled.section`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  max-width: var(--container);
+  margin: auto;
+`
+
+export const Heading = styled.h1`
+  font-size: var(--large);
+  margin-bottom: var(--large);
+`
+
+export const Body = styled.div`
+  // whatever you want...
+`
